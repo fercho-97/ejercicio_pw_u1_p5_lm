09c69bdfb9357a2c354f3460d121d1fa2d185d14
Fernando Mosquera <luismosquera97@gmail.com>
2023-12-06 19:47:43 -0500

    first commit

---
+++ js/ejercicio.js
@@ -0,0 +1,105 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+
+
+const mostrar1 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor1');
+    numero.innerText = txt;
+
+    
+}
+
+
+const mostrar2 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor2');
+    numero.innerText = txt;
+
+    
+}
+
+const mostrar3 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor3');
+    numero.innerText = txt;
+
+    
+}
+
+const mostrar4 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor4');
+    numero.innerText = txt;
+
+    
+}
+
+
+const mostrar5 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor5');
+    numero.innerText = txt;
+
+    
+}
+
+const mostrar6 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor6');
+    numero.innerText = txt;
+
+    
+}
+
+const mostrar7 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor7');
+    numero.innerText = txt;
+
+    
+}
+
+const mostrar8 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor8');
+    numero.innerText = txt;
+
+    
+}
+const mostrar9 = () => {
+
+    var txt= getRandomInt(10);
+
+    let numero = document.getElementById('idvalor9');
+    numero.innerText = txt;
+
+    
+}
+
+function mostrarX(elemento) {
+    elemento.innerHTML = 'X';
+}
+
+function ocultarX(elemento) {
+    elemento.innerHTML = '';
+}
